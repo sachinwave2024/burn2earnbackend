705cdebe3d7cb1668ec74ccc7073b4c04a359b06
sachinwave2024 <161458691+sachinwave2024@users.noreply.github.com>
2025-10-17 09:33:57 +0530

    Update jason_database.py

--- jason_wellness/jason_database.py
+++ jason_wellness/jason_database.py
@@ -4,26 +4,26 @@ def RELATIVE_PATH(*args):
     return os.path.join(BASE_DIR, *args)
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'Kiueayfafyawhfayrfhafahfea',
-#         'USER': 'burntoearn',
-#         'PASSWORD': 'YourStrongPassword123!',
-#         'HOST': '165.227.31.56',
-#         'PORT': '3306',
-#         'OPTIONS': {'charset': 'utf8mb4'},
-#     },
-#     'second_db': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'Wdiadkfaufdkaefadvd',
-#         'USER': 'burntoearn',
-#         'PASSWORD': 'YourStrongPassword123!',
-#         'HOST': '165.227.31.56',
-#         'PORT': '3306',
-#         'OPTIONS': {'charset': 'utf8mb4'},
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Kiueayfafyawhfayrfhafahfea',
+        'USER': 'burntoearn',
+        'PASSWORD': 'YourStrongPassword123!',
+        'HOST': '165.227.31.56',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
+    },
+    'second_db': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Wdiadkfaufdkaefadvd',
+        'USER': 'burntoearn',
+        'PASSWORD': 'YourStrongPassword123!',
+        'HOST': '165.227.31.56',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
+    }
+}
 
 
 
@@ -58,30 +58,30 @@ def RELATIVE_PATH(*args):
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Kiueayfafyawhfayrfhafahfea',
-        'USER': 'Lqwuuafjaifapoeruugaads',
-        'PASSWORD': 'diaekfaierka4eajgjuawajfz',
-        'HOST': '54.254.176.176',
-        'PORT': '3306',
-        'OPTIONS': {
-                'charset': 'utf8mb4',
-        },
-    },
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Kiueayfafyawhfayrfhafahfea',
+#         'USER': 'Lqwuuafjaifapoeruugaads',
+#         'PASSWORD': 'diaekfaierka4eajgjuawajfz',
+#         'HOST': '54.254.176.176',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#                 'charset': 'utf8mb4',
+#         },
+#     },
 
-    'second_db': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'Wdiadkfaufdkaefadvd',
-         'USER': 'Swuejdafuajfdawefaef',
-         'PASSWORD': 'Azefuafawefawefage',
-         'HOST': '3.0.28.172',
-         'PORT': '3306',
-         'OPTIONS': {
-                'charset': 'utf8mb4',
-          },
-     }
-}
+#     'second_db': {
+#          'ENGINE': 'django.db.backends.mysql',
+#          'NAME': 'Wdiadkfaufdkaefadvd',
+#          'USER': 'Swuejdafuajfdawefaef',
+#          'PASSWORD': 'Azefuafawefawefage',
+#          'HOST': '3.0.28.172',
+#          'PORT': '3306',
+#          'OPTIONS': {
+#                 'charset': 'utf8mb4',
+#           },
+#      }
+# }
 
 
